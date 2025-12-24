aa11083cb5e2ddbece37b3ee6e79f6b8fc92a3e2
Ashzuu <116486394+Ashzuu@users.noreply.github.com>
2025-12-24 15:05:34 +0100

    Initial commit of my commercial website

---
+++ app/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div id="header-logo-container">
+
+    </div>
+    <div id="header-menu-container">
+    </div>
+    <hr>
+  </header>
+</template>
+
+<style scoped>
+@import url("~/assets/css/components/header.css");
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>Bonjour ceci est un test</h1>
+  </main>
+</template>
+
+<style scoped>
+@import url("~/assets/css/pages/main-page.css");
+</style>
